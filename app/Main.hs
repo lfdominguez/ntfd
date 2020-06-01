802795d6561fef37426ec806edc1f39fd91b704b
Kamek <b.kamek@gmail.com>
2020-05-31 22:31:14 -0400

    owm support done

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,26 @@ import Control.Concurrent (threadDelay)
 import Control.Monad (forever, when)
 import Data.Bifunctor (first)
 import Data.Either (rights)
+import Data.Text (pack, Text)
 import Data.Time.Clock (NominalDiffTime)
 import DBus.Client
+    ( autoMethod
+    , connectSession
+    , defaultInterface
+    , requestName
+    , export
+    , readOnlyProperty
+    , interfaceName
+    , interfaceMethods
+    , interfaceProperties
+    , Client
+    , RequestNameReply(..)
+    )
 import System.Directory (getXdgDirectory, XdgDirectory(..))
 import System.Exit (exitFailure)
 
 import Config (loadConfig, Config(..), ConfigError(..), WeatherConfig(..))
+import Types.Weather (convert, Temperature(..), Unit(..))
 import qualified Stores.Weather as WS
 
 main :: IO ()
@@ -53,18 +67,51 @@ weatherService dbusClient config = do
   where
     interface s = defaultInterface
         { interfaceName       = "io.ntfd.openweathermap"
+        , interfaceMethods    = methods s
         , interfaceProperties = properties s
         }
-    renderedTemplate s = readOnlyProperty "RenderedTemplate" $ orEmpty $ WS.getRenderedTemplate s
-    properties s = [renderedTemplate s]
-
--- D-Bus has no null or empty type, this maps errors to mempty
-orEmpty :: Monoid t => IO (Either e t) -> IO t
-orEmpty query = do
-    res <- query
-    pure $ case res of
-        Left  _ -> mempty
-        Right t -> t
+    renderedTemplate s =
+        readOnlyProperty "RenderedTemplate" $ fromEither <$> WS.getRenderedTemplate s
+    currentIcon s = readOnlyProperty "CurrentIcon" $ fromIcon <$> WS.getCurrentIcon s
+    forecastIcon s = readOnlyProperty "ForecastIcon" $ fromIcon <$> WS.getForecastIcon s
+    currentTemp s = autoMethod "CurrentTemperature" $ currentTemperature s
+    forecastTemp s = autoMethod "ForecastTemperature" $ forecastTemperature s
+    methods s = [currentTemp s, forecastTemp s]
+    properties s = [renderedTemplate s, currentIcon s, forecastIcon s]
 
 sleep :: NominalDiffTime -> IO ()
 sleep = threadDelay . toMicroSeconds where toMicroSeconds = (1000000 *) . fromInteger . round
+
+-- The following functions helps mapping our data types to DBus
+fromEither :: (Monoid t) => Either e t -> t
+fromEither (Left  _) = mempty
+fromEither (Right t) = t
+
+fromIcon :: Maybe Char -> Text
+fromIcon Nothing  = ""
+fromIcon (Just c) = pack $ "" ++ [c]
+
+currentTemperature :: WS.Store c => c -> Text -> IO String
+currentTemperature client unit = do
+    current <- WS.getCurrentTemperature client
+    pure $ toTemperatureValue unit current
+
+forecastTemperature :: WS.Store c => c -> Text -> IO String
+forecastTemperature client unit = do
+    current <- WS.getForecastTemperature client
+    pure $ toTemperatureValue unit current
+
+toTemperatureValue :: Text -> Maybe Temperature -> String
+toTemperatureValue _ Nothing = ""
+toTemperatureValue u (Just t)
+    | u == "celcius"    = asValue Celcius
+    | u == "kelvin"     = asValue Kelvin
+    | u == "fahrenheit" = asValue Fahrenheit
+    | otherwise         = ""
+  where
+    value (Temperature v _) = v
+    asValue unit =
+        let
+            converted = convert t unit
+            rounded   = round $ value converted :: Integer
+        in show rounded
